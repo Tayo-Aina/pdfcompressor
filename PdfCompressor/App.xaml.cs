5d3880f97d2890ef4785d7063b744260dbcc8291
Tayo-Aina <tayodavidaina@gmail.com>
2026-08-05 08:52:22 +0100

    Apply saved theme at startup

--- PdfCompressor/App.xaml.cs
+++ PdfCompressor/App.xaml.cs
@@ -4,4 +4,9 @@ namespace PdfCompressor;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ThemeManager.Load();
+    }
 }
